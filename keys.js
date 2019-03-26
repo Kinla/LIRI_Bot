b25775c49c611c531730bc84dffab4dc5ae932f2
SharonC <sharon.s.c.chien@gmail.com>
2019-03-26 19:04:47 -0400

    moved key to dotenv file

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: "b32945b228cd448691a70ebe2eae3b42",
-  secret: "2f8d75a4d3f64925be3ccbdb59c95b29"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
