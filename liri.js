d3ca107fa5f2373aa996af8125d59900c2971e6b
SharonC <sharon.s.c.chien@gmail.com>
2019-03-26 01:43:17 -0400

    maybe done

--- liri.js
+++ liri.js
@@ -9,6 +9,28 @@ const fs = require("fs");
 const command = process.argv[2];
 const value = process.argv.splice(3).join("+");
 
+const writeLog = (res) => {
+    if (fs.existsSync("log.txt")) {
+        fs.appendFile("log.txt", "," + JSON.stringify(res), (err) =>{
+            if (err) throw err;
+        })
+    } else {
+        fs.appendFile("log.txt", JSON.stringify(res), (err) =>{
+            if (err) throw err;
+        })
+    }
+}
+
+const consoleLog = (res) => {
+    res.forEach(element => {
+        console.log("---------------------------------")
+        for (const key in element) {
+                const entry = element[key];
+                console.log(key + ": " + entry)
+        }                            
+    })
+}
+
 const concert = (title) => {
     let input = title.replace(/ |\+/g, "%20").replace(/"/g, "")
 
@@ -17,27 +39,33 @@ const concert = (title) => {
     axios
         .get(bandsURL)
         .then(function(response){
+            let results = [{"Command": "concert-this"}];
             if (!response.data.length){
-                console.log("Sorry, this artist has no upcoming show.")
+                const errMsg = {"Error": "Sorry, this artist has no upcoming show."}
+                results.push(errMsg)
             } else {
                 let show = response.data
-                for (let i = 0; i < response.data.length; i++){
+                for (let i = 0; i < show.length; i++){
                     let venue = show[i].venue.name;
                     let location = show[i].venue.city + ", " + show[i].venue.country;
                     let dT = show[i].datetime;
                     let date = moment(dT).format("LL")
-
-                    console.log("--------------------------------");
-                    console.log("Venue: " + venue);
-                    console.log("Location: " + location);
-                    console.log("Date: " + date);
+                    let result = {
+                        "Venue": venue,
+                        "Location": location,
+                        "Date": date
+                    }
+                    results.push(result)
                 }
+
             }
+            writeLog(results);             
+            consoleLog(results);                    
         })
         .catch(function(err){
             console.log("Oops! " + err)
         })  
-};
+}
 
 
 const song = (title) => {
@@ -48,17 +76,20 @@ const song = (title) => {
     spotify
         .search({ type: 'track', query: input, limit: 5 })
         .then(function(response) {
+            let results = [{"Command": "spotify-this-song"}];
             for (var i = 0; i < response.tracks.items.length; i++){
                 let artists = response.tracks.items[i].album.artists[0].name;
                 let name = response.tracks.items[i].name;
                 let preview = response.tracks.items[i].preview_url;
                 let album = response.tracks.items[i].album.name
-    
-                console.log("--------------------------------");
-                console.log("Artist(s): " + artists);
-                console.log("Song Title: " + name);
-                console.log("Preview: " + preview);
-                console.log("Album: " + album);
+
+                let result = {
+                    "Artist(s)": artists,
+                    "Title": name,
+                    "Preview": preview,
+                    "Album": album
+                }
+                results.push(result)    
             };
             if (!response.tracks.items.length){
                 spotify
@@ -68,16 +99,20 @@ const song = (title) => {
                     let name = response.name;
                     let preview = response.preview_url;
                     let album = response.album.name;
-        
-                    console.log("--------------------------------");
-                    console.log("Oops, that isn't a song we recognize. Try this number~")
-                    console.log(" ")
-                    console.log("Artist(s): " + artists);
-                    console.log("Song Title: " + name);
-                    console.log("Preview: " + preview);
-                    console.log("Album: " + album);
-                    })    
+
+                    let result = [{
+                        "Error": "Song not found! Try this song instead.",
+                        "Artist(s)": artists,
+                        "Title": name,
+                        "Preview": preview,
+                        "Album": album
+                    }]
+                    writeLog(result);             
+                    consoleLog(result);                
+                })    
             }
+            writeLog(results);             
+            consoleLog(results);                    
         })
         .catch(function(err){
             console.log("Oops! " + err)
@@ -101,15 +136,19 @@ const movie = (title) => {
             if (response.data.Response === "False"){
                 movie();
             } else {
-                console.log("--------------------------------");            
-                console.log("Title: " + response.data.Title);
-                console.log("Year: " + response.data.Year);
-                console.log("IMBD Rating: " + response.data.Ratings[0].Value);
-                console.log("Tomatoes Rating: " + response.data.Ratings[1].Value);
-                console.log("Country: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
+                let result = [{
+                    "Command": "movie-this",
+                    "Title": response.data.Title,
+                    "Year": response.data.Year,
+                    "IMBD_Rating": response.data.Ratings[0].Value,
+                    "Tomatoes_Rating": response.data.Ratings[1].Value,
+                    "Country": response.data.Country,
+                    "Language": response.data.Language,
+                    "Plot": response.data.Plot,
+                    "Actors": response.data.Actors
+                }]           
+                writeLog(result);             
+                consoleLog(result);    
             }
         })
         .catch(function(err){            
@@ -123,6 +162,11 @@ const simon = () => {
         input = data.split(",")
         controller(input[0],input[1])
     })
+
+    let result = [{"Command": "do-what-it-says"}];
+    writeLog(result);             
+    consoleLog(result);    
+
 
 }
 
